0cfdd611fe9484087214f002de91286487d8a9cb
ramadarshk <ramadarsh.3545@gmail.com>
2024-06-12 00:22:24 +0530

    see  data

--- app/src/main/java/com/example/imarticus_class_01/MainActivity.kt
+++ app/src/main/java/com/example/imarticus_class_01/MainActivity.kt
@@ -5,6 +5,11 @@ import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -26,12 +31,36 @@ class MainActivity : AppCompatActivity() {
       /*  //var dailIntent:Intent=Intent(Intent.ACTION_CALL, Uri.parse("tel:98664135435"))
         val webIntent:Intent=Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com/"))
         startActivity(webIntent)*/
-        var hintent:Intent=Intent(this,home::class.java)
-        startActivity(hintent)
-        Log.i("Log in page","Sign Up Button Clicked")
+
+        val name:EditText=findViewById(R.id.please_enter_name_here)
+        val pin:EditText=findViewById(R.id.enter_pin_here)
+        val age:EditText=findViewById(R.id.enter_age_here)
+        val gendervg:RadioGroup=findViewById(R.id.gender)
+        val mac=gendervg.checkedRadioButtonId
+        val ide=findViewById<RadioButton>(mac)
+        val checkbox1: CheckBox =findViewById(R.id.checkBox)
+        val checkbox2: CheckBox =findViewById(R.id.checkBox2)
+        val checkbox3: CheckBox =findViewById(R.id.checkBox3)
+        val checkbox4: CheckBox =findViewById(R.id.checkBox4)
+        val hobbies= retstr(checkbox1)+retstr(checkbox2)+retstr(checkbox3)+retstr(checkbox4)
+        val from1tohintent:Intent=Intent(this,home::class.java)
+        from1tohintent.putExtra("name",name.text.toString())
+        from1tohintent.putExtra("pin",pin.text.toString())
+        from1tohintent.putExtra("age",age.text.toString())
+        from1tohintent.putExtra("gender",ide.text.toString())
+        from1tohintent.putExtra("hobbies",hobbies)
+        startActivity(from1tohintent)
+        Log.i("Log in page", name.text.toString())
     }
 
     fun onclickcancel(view: View) {
         Log.i("Log in page","Cancel Button Clicked")
+    }
+}
+private fun retstr(cb: CheckBox): String {
+    if(cb.isChecked){
+        return "${cb.text.toString()} \n"}
+    else{
+        return ""
     }
 }
--- app/src/main/java/com/example/imarticus_class_01/home.kt
+++ app/src/main/java/com/example/imarticus_class_01/home.kt
@@ -1,6 +1,8 @@
 package com.example.imarticus_class_01
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,18 @@ class home : AppCompatActivity() {
             v.setPadding(systemBars.left , systemBars.top , systemBars.right , systemBars.bottom)
             insets
         }
+        val home:TextView=findViewById(R.id.name)
+        val pin:TextView=findViewById(R.id.pin)
+        val age:TextView=findViewById(R.id.age)
+        val genders:TextView=findViewById(R.id.genderv)
+        val hobbies:TextView=findViewById(R.id.hobbies)
+        val mes=intent.extras
+        home.text=mes?.getString("name")
+        pin.text=mes?.getString("pin")
+        age.text=mes?.getString("age")
+        genders.text=mes?.getString("gender")
+        hobbies.text=mes?.getString("hobbies")
+       //Log.i("data",data.toString())
+
     }
 }
